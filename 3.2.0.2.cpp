bdb0bd62ed7eb629cab7d2d83250cd356aeb111e
s24269 <92214376+SadScob@users.noreply.github.com>
2021-11-19 13:14:55 +0100

    Update 3.2.0.2.cpp

--- 3.2.0.2.cpp
+++ 3.2.0.2.cpp
@@ -2,8 +2,17 @@
 
 using namespace std;
 
-auto main (int argc, char** argv) -> int {
-    string a = "Hello, World!";
-    cout << a << endl;
+void swap(int &a, int &b) {
+    int temp = a;
+    a = b;
+    b=temp;
+
+}
+
+auto main(int argc, char** argv) -> int {
+    int a = 42, b = 64;
+    cout<< a << " " << b<<endl;
+    swap(a, b);
+    cout<< a << " " << b <<endl;
     return 0;
 }
